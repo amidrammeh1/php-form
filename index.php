c0dd86a99536519e7610c7e9e8b931d9e4ab72e6
amidrammeh1 <72460681+amidrammeh1@users.noreply.github.com>
2020-11-24 09:00:35 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,18 @@
 <?php
 //echo '<p>This is PHP!</p>';
 
-if (isset($_GET["FirstName"])) {//show info 
-
-
+if(isset($_GET["FirstName"])){//show feedback
+ echo $_GET["FirstName"];
 }else{//show form
-  echo '
-  <form action=''>
-  First Name: <input type="text" name="FirstName" /><br />
-  <input type="submit" />
+ echo '
+ <form action="">
+ First Name <input type="text" name="FirstName" />
+ <br />
+ <input type="submit">
   </form>
-  
-  
-  ';
-
+ ';
 }
+
 ?>
 
 
