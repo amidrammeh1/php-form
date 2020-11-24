75057c04fc5f7d108e460cb6bb6da5e5e43f8aeb
amidrammeh1 <72460681+amidrammeh1@users.noreply.github.com>
2020-11-24 09:26:18 -0800

    Update index.php

--- index.php
+++ index.php
@@ -5,15 +5,15 @@ if(isset($_GET["FirstName"])){//show feedback
  //echo $_GET["FirstName"];
  
  echo '<pre>';
-var_dump($GET);
+var_dump($_GET);
 echo '</pre>';
 
 
 }else{//show form
  echo '
  <form action="">
- First Name <input type="text" name="FirstName" />
- <br />
+ <p> First Name <input type="text" name="FirstName" /></p>
+ <p> Last Name <input type="text" name="LastName" /></p>
  <input type="submit">
   </form>
  ';
